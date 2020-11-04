4ff5395022abfb4204f20f0b5b47a29e22d7686f
jeyansaran620 <46715243+jeyansaran620@users.noreply.github.com>
2020-11-04 20:09:32 +0530

    server corrections

--- server/index.js
+++ server/index.js
@@ -4,9 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const uuid = require('uuid');
 
-// const captureWebsite = require('capture-website');
-
-const express = require('express')
+const express = require('express');
 
 const port = process.env.PORT || 3000;
 
@@ -14,7 +12,7 @@ const app = express();
 
 app.use(cors());
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(express.static('./public'));
 
@@ -22,27 +20,27 @@ const validateUrl = (str) =>
 {
     if (str.split(" ").length > 1)
     {
-    return false
+    return false;
     }
     
     const pattern =/^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 
   return Boolean(pattern.test(str));
-}
+};
 
 app.get('/download/:file',(req,res) => {
 
   const filePath = `${__dirname}/public/${req.params.file}`;
-  const type = req.params.file.split(".").slice(-1)[0]
-  const fileName = `screenshot.${type}`; // The default name the browser will use
+  const type = req.params.file.split(".").slice(-1)[0];
+  const fileName = `screenshot.${type}`; 
   res.download(filePath, fileName);  
 
-})
+});
 
 
 app.post('/shot/', (req,res) => {
 
- if (!validateUrl(req.body.url))
+ if (!validateUrl(req.body.url) || req.body.quality > 100 || req.body.quality < 0  || !(req.body.format === 'png' || req.body.format === 'jpg'))
  {
   res.statusCode = 400;
   res.send("verify the URL provided");
@@ -56,7 +54,7 @@ app.post('/shot/', (req,res) => {
     shotSize: {
       width: 1024,
        height: 'all'
-    }}
+    }};
 
  webshot(`${req.body.url}`,`${__dirname}/public/${fileName}`, options, function(err) {
       if(err)
@@ -66,26 +64,14 @@ app.post('/shot/', (req,res) => {
         
         return;
       }
-      // screenshot now saved to google.png
 
       const image = {
         url: fileName
-      }
+      };
       res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(image);
-    })
- 
- /*(async () => {
-	await captureWebsite.file(`${req.body.url}`, `${__dirname}/public/ready.${req.body.format}`, {
-    width: 1280,
-    height: 800,
-    fullPage: true,
-    quality: req.body.quality,
-    type: req.body.format
-	});
-})();
-*/
+    });
 });
 
 app.get('*' ,(req,res) => {
